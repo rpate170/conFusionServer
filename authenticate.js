538b6363c44359e7f6369aaffe88c27d6525278a
rpate170 <rushi.patel175@gmail.com>
2020-10-17 15:15:20 -0400

    Assignment 3

--- authenticate.js
+++ authenticate.js
@@ -35,3 +35,14 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => 
 }));
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+exports.verifyAdmin = function(req, res, next) {
+    if (req.user.admin) {
+        next();
+        return;
+    }
+    else {
+        var err = new Error('You are nopt authorized to perform this operation!');
+        err.status = 403;
+        return next(err);
+    }
+}
